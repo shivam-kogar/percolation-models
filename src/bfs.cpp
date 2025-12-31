d828371e116abbeddc6757774fc15f7991fb48e6
shivam-kogar <87800824+shivam-kogar@users.noreply.github.com>
2025-12-31 15:30:15 +0700

    Update bfs.cpp

--- src/bfs.cpp
+++ src/bfs.cpp
@@ -9,74 +9,25 @@ using namespace std;
 vector<int> reconstruct_path(int meeting_node, int id_start, int id_end, 
     const vector<int>& parents_start, const vector<int>& parents_end);
 
-/**
-struct AdaptedPath {
-    vector<int> path;
-    int actual_start;
-    int actual_end;
-};
+/* Implements a bidirectional BFS algorithm to find the shortest path between the starting node and the ending node
+'id_start' and 'id_end' are the one-dimensional IDs for the starting and ending lattice coordinates 
+'adj' is the adjacency list used to reconstruct the random lattice
+'n' is the dimension (or horizontal length + 1) of the lattice
+*/
+vector<int> bfs(int id_start, int id_end, const vector<vector<int>>& adj, int n) { 
+    
+    int N = n * n; // number of nodes after 2D lattice has been flattened, i.e. number of one-dimensional ID's
+    queue<int> to_visit_start; // queue of nodes to visit from the starting node
+    vector<int> parents_start(N, -1); 
+    vector<bool> visited_start(N, false); // vector indicating truth value of whether each node has been visited from the starting node
 
-vector<int> bfs(int id_start, int id_end, const vector<vector<int>>& adj, int n);
-vector<int> neighbors(int id, int n);
-
- AdaptedPath bfs_adaptive(int id_start, int id_end, const vector<vector<int>>& adj, int n) {
-    auto path = bfs(id_start, id_end, adj, n);
-    if (!path.empty()) return {path, id_start, id_end};
-
-    auto nbrs_start = neighbors(id_start, n);
-    for (int nbr : nbrs_start) {
-        auto path1 = bfs(nbr, id_end, adj, n);
-        if (!path1.empty()) return {path1, nbr, id_end};
-    }
-
-    auto nbrs_end = neighbors(id_end, n);
-    for (int nbr : nbrs_end) {
-        auto path2 = bfs(id_start, nbr, adj, n);
-        if (!path2.empty()) return {path2, id_start, nbr};
-    }
-
-    for (int nbr_start : nbrs_start) {
-        for (int nbr_end : nbrs_end) {
-            auto path3 = bfs(nbr_start, nbr_end, adj, n);
-            if (!path3.empty()) return {path3, nbr_start, nbr_end};
-        }
-    }
-
-    cerr << "No Path Exists, Even Between Neighbors!\n";
-    return {};
-} 
-
-vector<int> neighbors(int id, int n) {
-    vector<int> neighbors;
-    auto nbr = id_to_pair(id, n);
-    if (nbr.first - 1 > -1) {
-        neighbors.push_back(pair_to_id(nbr.first - 1, nbr.second, n));
-    }
-    if (nbr.first + 1 < n) {
-        neighbors.push_back(pair_to_id(nbr.first + 1, nbr.second, n));
-    }
-    if (nbr.second - 1 > -1) {
-        neighbors.push_back(pair_to_id(nbr.first, nbr.second - 1, n));
-    }
-    if (nbr.second + 1 < n) {
-        neighbors.push_back(pair_to_id(nbr.first, nbr.second + 1, n));
-    }
-    return neighbors;
-} **/
-
-vector<int> bfs(int id_start, int id_end, const vector<vector<int>>& adj, int n) {
-    int N = n * n; // number of nodes after 2D lattice has been flattened
-    queue<int> to_visit_start;
-    vector<int> parents_start(N, -1);
-    vector<bool> visited_start(N, false); 
-
-    queue<int> to_visit_end;
+    queue<int> to_visit_end; // queue of nodes to visit from the ending node
     vector<int> parents_end(N, -1);
-    vector<bool> visited_end(N, false); 
+    vector<bool> visited_end(N, false); // vector indicating truth value of whether each node has been visited from the ending node
 
     int meeting_node = -1;
 
-    bool found = false;
+    bool found = false; // boolean variable indicating whether the 
 
     to_visit_start.push(id_start);
     to_visit_end.push(id_end);
